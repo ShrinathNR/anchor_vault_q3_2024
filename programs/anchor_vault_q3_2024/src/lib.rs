e2d952e0d8a12aff80103fa2497fa3519b9eeaf5
Vitor Py Braga <12871+vitorpy@users.noreply.github.com>
2024-08-06 16:32:40 +0200

    feat: end of tuesday lesson, close method and initialize tests done

--- programs/anchor_vault_q3_2024/src/lib.rs
+++ programs/anchor_vault_q3_2024/src/lib.rs
@@ -22,6 +22,12 @@ pub mod anchor_vault_q3_2024 {
 
         Ok(())
     }
+
+    pub fn close(ctx: Context<Close>) -> Result<()> {
+        ctx.accounts.close()?;
+
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
@@ -133,6 +139,51 @@ impl<'info> Withdraw<'info> {
     }
 }
 
+
+#[derive(Accounts)]
+pub struct Close<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(
+        mut,
+        seeds = [b"vault", vault_state.key().as_ref()],
+        bump = vault_state.vault_bump,
+    )]
+    pub vault: SystemAccount<'info>,
+    #[account(
+        mut,
+        seeds = [b"state", user.key().as_ref()],
+        bump = vault_state.state_bump,
+        close = user,
+    )]
+    pub vault_state: Account<'info, VaultState>,
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> Close<'info> {
+    pub fn close(&mut self) -> Result<()> {
+        let cpi_program = self.system_program.to_account_info();
+
+        let cpi_accounts = Transfer {
+            from: self.vault.to_account_info(),
+            to: self.user.to_account_info(),
+        };
+
+        let seeds = &[
+            b"vault",
+            self.vault_state.to_account_info().key.as_ref(),
+            &[self.vault_state.vault_bump],
+        ];
+
+        let signer_seeds = &[&seeds[..]];
+
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer_seeds);
+
+        transfer(cpi_ctx, self.vault.to_account_info().lamports())?;
+
+        Ok(())
+    }
+}
 
 #[account]
 pub struct VaultState {
--- tests/anchor_vault_q3_2024.ts
+++ tests/anchor_vault_q3_2024.ts
@@ -4,13 +4,29 @@ import { AnchorVaultQ32024 } from "../target/types/anchor_vault_q3_2024";
 
 describe("anchor_vault_q3_2024", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.AnchorVaultQ32024 as Program<AnchorVaultQ32024>;
-
+  
+  const vaultState = anchor.web3.PublicKey.findProgramAddressSync([Buffer.from("state"), 
+    provider.publicKey.toBytes()], 
+    program.programId)[0];
+    
+  const vault = anchor.web3.PublicKey.findProgramAddressSync([Buffer.from("vault"), 
+    vaultState.toBytes()], 
+    program.programId)[0];
+    
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const tx = await program.methods.initialize().accountsPartial(
+      {
+        user: provider.wallet.publicKey,
+        vaultState,
+        vault,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      }
+    ).rpc();
     console.log("Your transaction signature", tx);
   });
 });

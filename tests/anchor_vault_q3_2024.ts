b8186a13273d605283891324afc6b4d6243298b2
Vitor Py Braga <12871+vitorpy@users.noreply.github.com>
2024-08-06 20:10:11 +0200

    test: cover tests for deposit, withdraw and close

--- tests/anchor_vault_q3_2024.ts
+++ tests/anchor_vault_q3_2024.ts
@@ -1,6 +1,7 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { AnchorVaultQ32024 } from "../target/types/anchor_vault_q3_2024";
+import { expect } from "chai";
 
 describe("anchor_vault_q3_2024", () => {
   // Configure the client to use the local cluster.
@@ -9,15 +10,17 @@ describe("anchor_vault_q3_2024", () => {
 
   const program = anchor.workspace.AnchorVaultQ32024 as Program<AnchorVaultQ32024>;
   
-  const vaultState = anchor.web3.PublicKey.findProgramAddressSync([Buffer.from("state"), 
+  const [vaultState] = anchor.web3.PublicKey.findProgramAddressSync([Buffer.from("state"), 
     provider.publicKey.toBytes()], 
-    program.programId)[0];
+    program.programId);
     
-  const vault = anchor.web3.PublicKey.findProgramAddressSync([Buffer.from("vault"), 
+  const [vault] = anchor.web3.PublicKey.findProgramAddressSync([Buffer.from("vault"), 
     vaultState.toBytes()], 
-    program.programId)[0];
+    program.programId);
     
-  it("Is initialized!", async () => {
+  it("Initialises", async () => {
+    await program.provider.connection.requestAirdrop(provider.wallet.publicKey, 1000000000);
+
     // Add your test here.
     const tx = await program.methods.initialize().accountsPartial(
       {
@@ -28,5 +31,50 @@ describe("anchor_vault_q3_2024", () => {
       }
     ).rpc();
     console.log("Your transaction signature", tx);
+  });
+
+  it("Deposit", async () => {
+    // Add your test here.
+    const amount = new anchor.BN(500000000);
+    const tx = await program.methods.deposit(amount).accountsPartial(
+      {
+        user: provider.wallet.publicKey,
+        vaultState,
+        vault,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      }
+    ).rpc();
+
+    console.log("Your transaction signature", tx);
+    expect((await provider.connection.getAccountInfo(vault)).lamports).to.be.equal(amount.toNumber());
+  });
+
+  it("Withdraw", async () => {
+    // Add your test here.
+    const amount = new anchor.BN(250000000);
+    const tx = await program.methods.withdraw(amount).accountsPartial(
+      {
+        user: provider.wallet.publicKey,
+        vaultState,
+        vault,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      }
+    ).rpc();
+    console.log("Your transaction signature", tx);
+    expect((await provider.connection.getAccountInfo(vault)).lamports).to.be.equal(amount.toNumber());
+  });
+
+  it("Close", async () => {
+    // Add your test here.
+    const tx = await program.methods.close().accountsPartial(
+      {
+        user: provider.wallet.publicKey,
+        vaultState,
+        vault,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      }
+    ).rpc();
+    console.log("Your transaction signature", tx);
+    expect((await provider.connection.getAccountInfo(vault))).to.be.null;
   });
 });
